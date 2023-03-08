ec3dd64882387d8d2706d2703243f6c091043170
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-08 09:18:13 +0700

    Create Navbar.js file

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <a href="/#">Home</a>
+                <a href="/#">Portfolio</a>
+                <a href="/#">My journey</a>
+                <a href="/#">About me</a>
+                <a href="/#">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
